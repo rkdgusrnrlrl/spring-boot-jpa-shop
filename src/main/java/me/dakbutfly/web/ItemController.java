a309f6ba5b8ea3a8eb0e751a7ae7fe018dad15d6
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2017-01-16 14:43:12 +0900

    상품 수정 폼 등록

--- src/main/java/me/dakbutfly/web/ItemController.java
+++ src/main/java/me/dakbutfly/web/ItemController.java
@@ -9,6 +9,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -41,5 +43,25 @@ public class ItemController {
         List<Item> items = itemService.findItems();
         model.addAttribute("items", items);
         return "items/itemList";
+    }
+
+    /**
+     * 상품 수정 폼
+     */
+    @RequestMapping(value = "/items/{itemId}/edit", method = RequestMethod.GET)
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+
+        Item item = itemService.findItemById(itemId);
+        model.addAttribute("item", item);
+        return "items/updateItemForm";
+    }
+
+    /**
+     * 상품 수정
+     */
+    @RequestMapping(value = "/items/{itemId}/edit", method = RequestMethod.POST)
+    public String updateItem(@ModelAttribute("item") Item item) {
+        itemService.saveItem(item);
+        return "redirect:/items";
     }
 }
